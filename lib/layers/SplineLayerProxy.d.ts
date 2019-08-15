084970f5baf8da9d5b87cbc27e44c11f2bb30c83
Arno Fiva <arnofiva@gmail.com>
2019-08-15 14:49:06 +0200

    Deploy latest changes

--- lib/layers/SplineLayerProxy.d.ts
+++ lib/layers/SplineLayerProxy.d.ts
@@ -3,15 +3,19 @@ import Accessor = require("esri/core/Accessor");
 import * as Graphic from "esri/Graphic";
 import * as FeatureLayer from "esri/layers/FeatureLayer";
 import * as GeoJSONLayer from "esri/layers/GeoJSONLayer";
-declare type lineLayer = FeatureLayer | GeoJSONLayer;
+declare type LineLayerType = FeatureLayer | GeoJSONLayer;
 declare const SplineLayerProxy_base: typeof Accessor;
 declare class SplineLayerProxy extends SplineLayerProxy_base {
-    lineLayer: lineLayer;
-    splineLayer: FeatureLayer;
+    lineLayer: LineLayerType;
+    private resolveSplineLayer;
+    private rejectSplineLayer;
+    private splineLayerPromise;
     private existingObjectIds;
     getLineGraphic(splineGraphic: Graphic): IPromise<Graphic>;
+    whenSplineLayer(): IPromise<FeatureLayer>;
     private createSplineGraphic;
     private initializeSplineLayer;
-    private refreshSplines;
+    private refresh;
+    private refreshSplineLayer;
 }
 export = SplineLayerProxy;
