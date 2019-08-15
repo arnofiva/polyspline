a201eaa23e94f7f6683099591273f0a8afa954dd
Arno Fiva <arnofiva@gmail.com>
2019-08-15 13:41:19 +0200

    Rebuilt JS library

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,1 +1,7 @@
-export {};
+import Spline = require("./geometry/Spline");
+import SplineLayerProxy = require("./layers/SplineLayerProxy");
+declare const _default: {
+    Spline: typeof Spline;
+    SplineLayerProxy: typeof SplineLayerProxy;
+};
+export = _default;
